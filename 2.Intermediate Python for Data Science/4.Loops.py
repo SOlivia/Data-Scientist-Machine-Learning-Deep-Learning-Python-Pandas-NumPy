78f9165812b68a7ab9ceb9a4bd16b41a14f8b134
MUCHCHIMA SUPHAP <22536728+SOlivia@users.noreply.github.com>
2017-12-26 23:27:31 -0600

    Create 4.Loops.py

---
+++ 2.Intermediate Python for Data Science/4.Loops.py
@@ -0,0 +1,108 @@
+# Initialize offset
+offset = 8
+
+# Code the while loop
+while offset != 0 :
+    print("correcting...")
+    offset = offset - 1
+    print(offset)
+  
+# Initialize offset
+offset = -6
+
+# Code the while loop
+while offset != 0 :
+    print("correcting...")
+    if offset > 0 :
+        offset = offset - 1
+    else :
+        offset = offset + 1
+    print(offset)
+    
+# areas list
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Code the for loop
+for a in areas :
+    print(a)
+    
+# areas list
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Change for loop to use enumerate()
+for index, a in enumerate(areas) :
+    print("room " + str(index) + ": " + str(a))
+    
+# areas list
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Code the for loop
+for index, area in enumerate(areas) :
+    print("room " + str(index + 1) + ": " + str(area))
+    
+# house list of lists
+house = [["hallway", 11.25], 
+         ["kitchen", 18.0], 
+         ["living room", 20.0], 
+         ["bedroom", 10.75], 
+         ["bathroom", 9.50]]
+         
+# Build a for loop from scratch
+for h in house :
+    print("the " + str(h[0]) + " is "  + str(h[1]) + " sqm")
+    
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'bonn', 
+          'norway':'oslo', 'italy':'rome', 'poland':'warsaw', 'australia':'vienna' }
+          
+# Iterate over europe
+for key, value in europe.items() :
+    print("the capital of " + str(key) + " is " + str(value))
+    
+# Import numpy as np
+import numpy as np
+
+# For loop over np_height
+for height in np_height :
+    print(str(height) + " inches")
+
+# For loop over np_baseball
+for baseball in np.nditer(np_baseball) :
+    print(str(baseball))
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Iterate over rows of cars
+for lab, row in cars.iterrows() :
+    print(lab)
+    print(row)
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Adapt for loop
+for lab, row in cars.iterrows() :
+    print(lab + ": " + str(row['cars_per_cap']))
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Code for loop that adds COUNTRY column
+for lab, row in cars.iterrows() :
+    cars.loc[lab, "COUNTRY"] = row["country"].upper()
+
+
+# Print cars
+print(cars)
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv("cars.csv", index_col = 0)
+
+# Use .apply(str.upper)
+cars["COUNTRY"] = cars["country"].apply(str.upper)
+print(cars)
